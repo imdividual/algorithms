b02baae02a1454a6e6bfc3d8ea54e6c5bab74f54
imdividual <dayouliu0@gmail.com>
2020-08-15 11:52:21 -0400

    updated problems

--- bt-preorder-inorder/main.cpp
+++ bt-preorder-inorder/main.cpp
@@ -9,52 +9,22 @@ struct TreeNode {
     TreeNode *right;
 };
 
-TreeNode* bt_preorder_inorder(
-        vector<int>& preorder, vector<int>& inorder,
-        unordered_map<int, int> &preindex, unordered_map<int, int> &inindex,
-        int p1, int p2, int i1, int i2) {
-
-    if(p1 == p2) return new TreeNode {preorder[p1], nullptr, nullptr};
-
-    int val = preorder[p1];
-    int mid = inindex[val];
-    int end = mid != i1 ? preindex[inorder[mid-1]] : p1;
-
-    TreeNode *left = nullptr;
-    TreeNode *right = nullptr;
-
-    int pl1 = p1+1;
-    int pl2 = end;
-    int pr1 = end+1;
-    int pr2 = p2;
-    int il1 = i1;
-    int il2 = mid-1;
-    int ir1 = mid+1;
-    int ir2 = i2;
-
-    if(mid != i1) {
-        left = bt_preorder_inorder(preorder, inorder, preindex, inindex, pl1, pl2, il1, il2);
-    }
-
-    if(mid != i2) {
-        right = bt_preorder_inorder(preorder, inorder, preindex, inindex, pr1, pr2, ir1, ir2);
-    }
-
-    return new TreeNode{val, left, right};
+TreeNode* bt_preorder_inorder(vector<int>& preorder, vector<int>& inorder,
+                              unordered_map<int, int>& find, int &pre1, int in1, int in2) {
+    if(pre1 >= preorder.size() || in1 > in2) return nullptr;
+    int root = preorder[pre1];
+    int index = find[root];
+    ++pre1;
+    TreeNode *left = bt_preorder_inorder(preorder, inorder, find, pre1, in1, index-1);
+    TreeNode *right = bt_preorder_inorder(preorder, inorder, find, pre1, index+1, in2);
+    return new TreeNode{root, left, right};
 }
 
 TreeNode* bt_preorder_inorder(vector<int>& preorder, vector<int>& inorder) {
-    if(preorder.empty()) return nullptr;
-    unordered_map<int, int> preindex, inindex;
-    for(int i = 0; i < preorder.size(); ++i) {
-        preindex[preorder[i]] = i;
-        inindex[inorder[i]] = i;
-    }
-    return bt_preorder_inorder(
-            preorder, inorder,
-            preindex, inindex,
-            0, preorder.size()-1,
-            0, inorder.size()-1);
+    unordered_map<int, int> find;
+    for(int i = 0; i < inorder.size(); ++i) find[inorder[i]] = i;
+    int pre1 = 0;
+    return bt_preorder_inorder(preorder, inorder, find, pre1, 0, inorder.size()-1);
 }
 
 int main() {
