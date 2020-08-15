e0bd718a3ed2e78a4481da42925ef81e65313709
imdividual <dayouliu0@gmail.com>
2020-08-15 16:56:42 -0400

    updated problems

--- dp-knapsack01/main.cpp
+++ dp-knapsack01/main.cpp
@@ -1,25 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp_knapsack01(vector<int> w, vector<int> v, int W) {
+void dp_knapsack01(vector<int> &sack, int W, vector<int> w, vector<int> v) {
     int dp[w.size()+1][W+1];
     for(int i = 0; i <= w.size(); ++i) {
         for(int j = 0; j <= W; ++j) {
             if(i == 0 || j == 0) {
                 dp[i][j] = 0;
-            } else if(j < w[i]) {
+            } else if(j < w[i-1]) {
                 dp[i][j] = dp[i-1][j];
             } else {
-                dp[i][j] = max(dp[i-1][j], v[i] + dp[i-1][j-w[i]]);
+                dp[i][j] = max(dp[i-1][j], v[i-1] + dp[i-1][j-w[i-1]]);
             }
         }
     }
-    return dp[w.size()][W];
+
+    // backtrack to find items
+    int i = w.size();
+    int j = W;
+    while(i > 0 && j > 0) {
+        if(j >= w[i-1] && dp[i][j]-v[i-1] == dp[i-1][j-w[i-1]]) {
+            sack.emplace_back(i);
+            j -= w[i-1];
+        }
+        --i;
+    }
 }
 
 int main() {
     vector<int> w{10, 20, 30};
     vector<int> v{60, 100, 120};
-    cout << dp_knapsack01(w, v, 50) << endl;
+    vector<int> sack;
+    dp_knapsack01(sack, 50, w, v);
+    sort(sack.begin(), sack.end());
+    for(auto i : sack) cout << i << endl;
     return 0;
 }
