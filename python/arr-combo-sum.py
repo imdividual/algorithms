c84ecee6b066dd519b113973c43d6cd8a9e34067
dayouliu <dayouliu0@gmail.com>
2021-01-13 18:13:04 -0500

    added problems

--- python/arr-combo-sum.py
+++ python/arr-combo-sum.py
@@ -16,7 +16,32 @@ class Solution:
         lis = []
         self.recurse(candidates, target, los, lis, 0, 0)
         return los
+    
+    def recurse2(self, candidates, target, los, lis, sm, i):
+        if sm == target:
+            los.append(lis[:])
+        elif i < len(candidates):
+            n = candidates[i]
+            if sm + n <= target:
+                lis.append(n)
+                self.recurse2(candidates, target, los, lis, sm+n, i+1)
+                lis.pop()
+            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i += 1  
+            self.recurse2(candidates, target, los, lis, sm, i+1)
+      
+    def combinationSum2(self, candidates, target):
+        los = []
+        lis = []
+        candidates.sort()
+        self.recurse2(candidates, target, los, lis, 0, 0)
+        return los
+
         
 test = Solution()
+
 los = test.combinationSum([2,3,5], 8)
+print(los)
+
+los = test.combinationSum2([1,1,1,1,2,2,2,2,6], 8)
 print(los)
